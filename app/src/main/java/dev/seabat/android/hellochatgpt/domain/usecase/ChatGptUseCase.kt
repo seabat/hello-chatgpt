efa5ca652c946aaee53fd7b330d87c920f21aac0
seabat <ryouta.nakanishi.86@gmail.com>
2023-03-04 22:55:22 +0900

    usecase レイアを作成

---
+++ app/src/main/java/dev/seabat/android/hellochatgpt/domain/usecase/ChatGptUseCase.kt
@@ -0,0 +1,32 @@
+package dev.seabat.android.hellochatgpt.domain.usecase
+
+import dev.seabat.android.hellochatgpt.data.repo.openai.OpenAiRepository
+import dev.seabat.android.hellochatgpt.data.source.openai.OpenAiRequest
+import dev.seabat.android.hellochatgpt.data.source.openai.createOpenAiRequestJson
+import dev.seabat.android.hellochatgpt.model.net.MEDIA_TYPE_JSON
+import dev.seabat.android.hellochatgpt.model.net.TEXT_DAVINCI_003
+import okhttp3.RequestBody
+import javax.inject.Inject
+
+class ChatGptUseCase @Inject constructor(private val repository: OpenAiRepository) {
+    suspend fun sendMessage(query: String): String {
+        val requestBody = RequestBody.create(
+            MEDIA_TYPE_JSON,
+            createOpenAiRequestJson(
+                OpenAiRequest(
+                    TEXT_DAVINCI_003,
+                    query,
+                    100,
+                    0f,
+//                    1f,
+//                    1,
+//                    false,
+//                    null,
+//                    "\n"
+                )
+            )
+        )
+        val response = repository.getPrompt(requestBody)
+        return response.choices[0].text
+    }
+}
